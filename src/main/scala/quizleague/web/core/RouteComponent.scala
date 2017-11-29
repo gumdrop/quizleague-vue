39bc5935089909001fa0a58ef9d1fb73af0db307
gumdrop <davidcgood@gmail.com>
2017-11-29 11:38:59 +0000

    tweaked

--- src/main/scala/quizleague/web/core/RouteComponent.scala
+++ src/main/scala/quizleague/web/core/RouteComponent.scala
@@ -10,53 +10,49 @@ import rxscalajs.Subject
 import rxscalajs.subjects.ReplaySubject
 
 trait RouteComponent {
-  
-  def component:js.Dynamic
-  
+
+  def component: js.Dynamic
+
   def apply() = component
-  
+
 }
 
-trait PageComponent{
-  def apply():js.Any
+trait PageComponent {
+  def apply(): js.Any
 }
 
-trait Component{
-  
-  val name:String
-  def template:String
-  def props:js.Array[String] = @@()
-  def watch:Map[String, js.Dynamic => Unit] = Map()
-  def subParams:Map[String,String] = Map()
-  def subscriptions:Map[String, js.Dynamic => Observable[Any]] = Map()
-  def data:js.Dynamic => Map[String, js.Any] = Map()
-  def methods:Map[String,js.Any] = Map()
-  
+trait Component {
+
+  val name: String
+  def template: String
+  def props: js.Array[String] = @@()
+  def watch: Map[String, js.Dynamic => Unit] = Map()
+  def subParams: Map[String, String] = Map()
+  def subscriptions: Map[String, js.Dynamic => Observable[Any]] = Map()
+  def data: js.Dynamic => Map[String, js.Any] = Map()
+  def methods: Map[String, js.Any] = Map()
+
   def apply() = {
-    
-    def sw(fn:js.Dynamic => Observable[Any]) = update(ReplaySubject())(fn) _ 
-    
-    val subs =  subscriptions.map{case(k,v) => (k, sw(v))}
-    
-    val subwatches = subParams.map{case(k,v) => (k, subs(v):js.ThisFunction)}
-    
 
-    
-    
-    
-    literal(
-  
-    template = template,
-    props = props,
-    watch = (watch.map{ case(k,v) => (k,v:js.ThisFunction)} ++ subwatches).toJSDictionary,
-    subscriptions = ((c:js.Dynamic) => subs.map{case(k,v) => (k,v(c))}.toJSDictionary):js.ThisFunction,
-    data = ((v:js.Dynamic) => data(v).toJSDictionary):js.ThisFunction,
-    methods = methods.toJSDictionary
-  )
-  }
-  
-  def update(subject: Subject[Any])(fn:js.Dynamic => Observable[Any])(c:js.Dynamic) = {
+    def update(subject: Subject[Any])(fn: js.Dynamic => Observable[Any])(c: js.Dynamic) = {
       fn(c).subscribe(ve => subject.next(ve))
       subject.inner
+    }
+
+    def sw(fn: js.Dynamic => Observable[Any]) = update(ReplaySubject())(fn) _
+
+    val subs = subscriptions.map { case (k, v) => (k, sw(v)) }
+
+    val subwatches = subParams.map { case (k, v) => (k, subs(v): js.ThisFunction) }
+
+    literal(
+
+      template = template,
+      props = props,
+      watch = (watch.map { case (k, v) => (k, v: js.ThisFunction) } ++ subwatches).toJSDictionary,
+      subscriptions = ((c: js.Dynamic) => subs.map { case (k, v) => (k, v(c)) }.toJSDictionary): js.ThisFunction,
+      data = ((v: js.Dynamic) => data(v).toJSDictionary): js.ThisFunction,
+      methods = methods.toJSDictionary)
   }
+
 }
