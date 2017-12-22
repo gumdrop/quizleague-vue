fba096afc632b009e73bbbc30ec302cd9d29d7fd
gumdrop <davidcgood@gmail.com>
2017-12-22 17:51:40 +0000

    more recalc

--- src/main/scala/quizleague/domain/util/LeagueTableRecalculator.scala
+++ src/main/scala/quizleague/domain/util/LeagueTableRecalculator.scala
@@ -13,20 +13,26 @@ class LeagueTableRecalculator(
     ) yield(team, fixture)
     
     val map = things.groupBy(_._1).map { case (k,v) => (k,v.map(_._2))}
+     
+    def points(home:Boolean)(fixture:Fixture) = {
+        val score1 = if(home)fixture.result.get.homeScore else fixture.result.get.awayScore
+        val score2 = if(home)fixture.result.get.awayScore else fixture.result.get.homeScore
+        if(score1 > score2) 2 else if (score1 == score2) 1 else 0
+    }
     
     val scores = map.map{case(team,fixtures) => {
-      val homeTotal = fixtures.filter(_.home == team).map(fixture =>  {
-        val homeScore = fixture.result.get.homeScore
-        val awayScore = fixture.result.get.awayScore
-        if(homeScore > awayScore) 2 else if (homeScore == awayScore) 1 else 0})
+      val homeFixtures = fixtures.filter(_.home == team)
+      val awayFixtures = fixtures.filter(_.away == team)
       
-      val awayTotal = fixtures.filter(_.away == team).map(fixture =>  {
-        val homeScore = fixture.result.get.homeScore
-        val awayScore = fixture.result.get.awayScore
-        if(homeScore < awayScore) 2 else if (homeScore == awayScore) 1 else 0})
-        
+      val homeTotal = homeFixtures.map(points(true) _).sum
+      
+      val awayTotal = awayFixtures.map(points(false) _).sum
+      
+      val others = fixtures.filter
       ((team, homeTotal + awayTotal))  
     }}
+    
+ 
   }
   
 }
