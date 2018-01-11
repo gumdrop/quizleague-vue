20145aa0136bc233155fa66fc1a3ffdcf6ee0f57
gumdrop <davidcgood@gmail.com>
2018-01-11 16:22:55 +0000

    tweaks

--- src/main/scala/quizleague/web/site/venue/VenuesComponent.scala
+++ src/main/scala/quizleague/web/site/venue/VenuesComponent.scala
@@ -3,23 +3,6 @@ package quizleague.web.site.venue
 import quizleague.web.core.RouteComponent
 import quizleague.web.core.GridSizeComponentConfig
 
-//@Component(
-//  template = """<ql-named-text name="venues-front-page"></ql-named-text>"""    
-//)
-//@classModeScala
-//class VenuesComponent(
-//    override val titleService:TitleService,
-//    override val sideMenuService:SideMenuService) extends SectionComponent with MenuComponent with TitledComponent{
-//  
-//    setTitle("Venues")
-//}
-//
-//@Component(
-//  template = """<ql-section-title><span>Venues</span></ql-section-title>"""    
-//)
-//class VenuesTitleComponent {
-//  
-//}
 
 object VenuesComponent extends RouteComponent with GridSizeComponentConfig{
    override val template="""
